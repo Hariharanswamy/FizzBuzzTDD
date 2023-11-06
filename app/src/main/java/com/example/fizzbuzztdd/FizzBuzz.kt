a863e871ca3712ab80af99884b2a0d560fc0f92a
Hari Haran Swamy <hariharan.swamy2@gmail.com>
2023-11-06 16:45:26 +0530

    covered test cases in test class

--- app/src/main/java/com/example/fizzbuzztdd/FizzBuzz.kt
+++ app/src/main/java/com/example/fizzbuzztdd/FizzBuzz.kt
@@ -1,4 +1,12 @@
 package com.example.fizzbuzztdd
 
 class FizzBuzz {
+
+    fun convertNumber(num: Int): String {
+        return ""
+    }
+
+    fun getConvertedList(): List<String> {
+        return mutableListOf<String>()
+    }
 }
--- app/src/test/java/com/example/fizzbuzztdd/FizzBuzzTest.kt
+++ app/src/test/java/com/example/fizzbuzztdd/FizzBuzzTest.kt
@@ -1,23 +1,67 @@
 package com.example.fizzbuzztdd
 
-import org.junit.Before
+import org.junit.Assert
+import org.junit.Test
 
 
 class FizzBuzzTest {
 
-    @Before
-    fun setUp() {
+    private val fizzBuzz = FizzBuzz()
+
+    /**
+     * Testing a number like 1,2 which are not divisible by 3 and 5
+     */
+    @Test
+    fun convertNumber_NotDivisibleBy3And5_SameNumber() {
+        val convertNumber = fizzBuzz.convertNumber(1)
+        Assert.assertEquals("1", convertNumber)
+        val convertNumber2 = fizzBuzz.convertNumber(8)
+        Assert.assertEquals("8", convertNumber2)
     }
 
-     // Testing a number like 1,2 which are not divisible by 3 and 5
+    /**
+     * Testing a number which are divisible by 3 but not 5
+     */
+    @Test
+    fun convertNumber_DivisibleBy3_FizzReturned(){
+        val convertNumber = fizzBuzz.convertNumber(3)
+        Assert.assertEquals("Fizz", convertNumber)
+        val convertNumber2 = fizzBuzz.convertNumber(9)
+        Assert.assertEquals("Fizz", convertNumber2)
+    }
 
-     // Testing a number which are divisible by 3 but not 5
+    /**
+     * Testing a number which are divisible by 5 but not 3
+     */
+    @Test
+    fun convertNumber_DivisibleBy5_BuzzReturned(){
+        val convertNumber = fizzBuzz.convertNumber(5)
+        Assert.assertEquals("Buzz", convertNumber)
+        val convertNumber2 = fizzBuzz.convertNumber(25)
+        Assert.assertEquals("Buzz", convertNumber2)
+    }
 
-     // Testing a number which are divisible by 5 but not 3
+    /**
+     * Testing a number which are divisible by 3 and 5
+     */
+    @Test
+    fun convertNumber_DivisibleBy3And5_FizzBuzzReturned(){
+        val convertNumber = fizzBuzz.convertNumber(15)
+        Assert.assertEquals("FizzBuzz", convertNumber)
+        val convertNumber2 = fizzBuzz.convertNumber(45)
+        Assert.assertEquals("FizzBuzz", convertNumber2)
+    }
 
-     // Testing a number which are divisible by 3 and 5
-
-     // Testing in the list if all conditions are met
-
+    /**
+     * Testing in the list if all conditions are met
+     */
+    @Test
+    fun convertList_FizzBuzzReturned(){
+        val list = fizzBuzz.getConvertedList()
+        Assert.assertEquals("8", list[7])
+        Assert.assertEquals("Fizz", list[8])
+        Assert.assertEquals("Buzz", list[9])
+        Assert.assertEquals("FizzBuzz", list[14])
+    }
 
 }
