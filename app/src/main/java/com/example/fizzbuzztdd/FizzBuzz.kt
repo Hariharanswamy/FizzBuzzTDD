376458971fa8d927fa28d17950f134308eef2253
Hari Haran Swamy <hariharan.swamy2@gmail.com>
2023-11-06 16:37:10 +0530

    listed possible test cases in test class

---
+++ app/src/main/java/com/example/fizzbuzztdd/FizzBuzz.kt
@@ -0,0 +1,4 @@
+package com.example.fizzbuzztdd
+
+class FizzBuzz {
+}
---
+++ app/src/test/java/com/example/fizzbuzztdd/FizzBuzzTest.kt
@@ -0,0 +1,23 @@
+package com.example.fizzbuzztdd
+
+import org.junit.Before
+
+
+class FizzBuzzTest {
+
+    @Before
+    fun setUp() {
+    }
+
+     // Testing a number like 1,2 which are not divisible by 3 and 5
+
+     // Testing a number which are divisible by 3 but not 5
+
+     // Testing a number which are divisible by 5 but not 3
+
+     // Testing a number which are divisible by 3 and 5
+
+     // Testing in the list if all conditions are met
+
+
+}
