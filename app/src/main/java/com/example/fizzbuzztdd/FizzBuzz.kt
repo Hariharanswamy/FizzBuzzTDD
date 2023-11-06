c8eaa9fbd0dd41f77ed83ac4281b1178a685cf24
Hari Haran Swamy <hariharan.swamy2@gmail.com>
2023-11-06 16:47:09 +0530

    implemented fizzbuzz logic all test cases are passed

--- app/src/main/java/com/example/fizzbuzztdd/FizzBuzz.kt
+++ app/src/main/java/com/example/fizzbuzztdd/FizzBuzz.kt
@@ -2,11 +2,26 @@ package com.example.fizzbuzztdd
 
 class FizzBuzz {
 
-    fun convertNumber(num: Int): String {
-        return ""
+    fun getConvertedList(): List<String> {
+        val list = mutableListOf<String>()
+        for (i in 1..100) {
+            list.add(convertNumber(i))
+        }
+        return list
     }
 
-    fun getConvertedList(): List<String> {
-        return mutableListOf<String>()
+
+    fun convertNumber(num: Int): String {
+        val result = num.toString()
+        if (num > 0) {
+            if (num % 3 == 0 && num % 5 == 0) {
+                return "FizzBuzz"
+            } else if (num % 3 == 0) {
+                return "Fizz"
+            } else if (num % 5 == 0) {
+                return "Buzz"
+            }
+        }
+        return result
     }
 }
