cf21ad280a4d86c117a067a6bfe83b80556bd8fb
Hari Haran Swamy <hariharan.swamy2@gmail.com>
2023-11-06 16:51:46 +0530

    ui recyclerview and adapter created

--- app/src/main/java/com/example/fizzbuzztdd/MainActivity.kt
+++ app/src/main/java/com/example/fizzbuzztdd/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.fizzbuzztdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerview = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerview.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/example/fizzbuzztdd/RecyclerAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.fizzbuzztdd
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter(private val dataSet: List<String>) :
+    RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+        init {
+            textView = view.findViewById(R.id.string)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.list_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.textView.text = dataSet[position]
+    }
+
+    override fun getItemCount() = dataSet.size
+
+}
